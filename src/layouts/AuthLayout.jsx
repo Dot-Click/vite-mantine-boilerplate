c1acb9e679fad4876a31d88bef4b2a850b86038b
zubair arif <dotclickpostman@gmail.com>
2024-02-12 13:57:03 +0500

    vite boiler plate

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default AuthLayout;
